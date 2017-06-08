a7516d14119ca5328aa1331905ba86e87d8444f9
Kelwin Lee <kelwin.lee@gmail.com>
2017-06-09 00:05:33 +0800

    Modified controller to use scraper

--- src/main/java/com/example/cuzziepetrolapp/controller/PetrolController.java
+++ src/main/java/com/example/cuzziepetrolapp/controller/PetrolController.java
@@ -1,11 +1,10 @@
 package com.example.cuzziepetrolapp.controller;
 
 import com.example.cuzziepetrolapp.model.Petrol;
+import com.example.cuzziepetrolapp.util.PetrolPriceScraper;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,9 +15,7 @@ public class PetrolController {
 
     @GetMapping("/")
     public List<Petrol> getPetrolPrice() {
-        List<Petrol> petrolList = new ArrayList<>();
-        petrolList.add(new Petrol("Ron 95", new BigDecimal("2.05"), "-0.05 compare previous week"));
-        petrolList.add(new Petrol("Ron 97", new BigDecimal("2.15"), "blablabla"));
+        List<Petrol> petrolList = PetrolPriceScraper.scrapePetrolPrice();
         return petrolList;
     }
 
